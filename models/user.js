5bdabfcbd329b60abbb467789e2c62d8b9d91fbd
Rogerndaba <rogerndaba@gmail.com>
2018-11-20 17:53:56 +0200

    yeaaaaaaaaaaaaaah

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 // User Schema
 const UserSchema = mongoose.Schema({
     firstname: {
@@ -25,3 +25,12 @@ const UserSchema = mongoose.Schema({
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.createUser = function(newUser, callback) {
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
