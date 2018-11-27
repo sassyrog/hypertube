91e35171115f909dd3bbe53c1b14e78b5a737e28
Rogerndaba <rogerndaba@gmail.com>
2018-11-27 21:00:23 +0200

    gar has started

--- routes/update.js
+++ routes/update.js
@@ -14,44 +14,26 @@ router.post('/', (req, res) => {
     const password1 = req.body.password1;
     const password2 = req.body.password2;
 
+    User.findById('5bf42bae9191be2086e4d0a8', function(err, user) {
+        console.log(user);
+    })
     User.findOne({
-        username: {
-            "$regex": "^" + username + "\\b",
-            "$options": "i"
-        }
+        $or: [{
+            'email': email
+        }, {
+            'username': username
+        }]
     }, function(err, user) {
-        User.findOne({
-            email: {
-                "$regex": "^" + email + "\\b",
-                "$options": "i"
-            }
-        }, function(err, mail) {
-            if (user || mail) {
-                if (user) {
-                    res.render('profile_update', {
-                        user: 'username already taken'
-                    });
-                } else if (mail) {
-                    res.render('profile_update', {
-                        mail: 'email already exists'
-                    });
-                }
-            }
-        });
+        if (user && user.email == email)
+            console.log('email');
+        else if (user && user.username == username) {
+            console.log('username');
+        }
     });
 
-
-    // User.findById('5bf42bae9191be2086e4d0a8', function(err, user) {
-    //     console.log(req.body);
-    //     if ('username' in req.body)
-    //         console.log('yeah');
-    //     console.log();
-    //     user.firstname = 'roger';
-    //     user.lastname = 'ndaba';
-    //     user.save();
-    //     console.log(user);
-    // })
     res.redirect('/test');
+
+
 })
 
 module.exports = router;
